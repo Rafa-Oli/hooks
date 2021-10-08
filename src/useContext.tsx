d5300c7a44ee20fa6b20799a83674cb482b1c09f
Rafaela Oliveira <rafaela.albaniza@gmail.com>
2021-10-08 20:36:20 -0300

    refactor: refactor useContext

--- src/useContext.tsx
+++ src/useContext.tsx
@@ -1,49 +1,12 @@
-import React, { useContext, useState } from 'react';
-
-const globalState = {
-  title: 'The title of context',
-  counter: 0,
-  body: 'body of context',
-};
-
-const GlobalContext = React.createContext(undefined);
-
-const Div = () => {
-  return (
-    <>
-      <H1 />
-      <P />
-    </>
-  );
-};
-
-const H1 = () => {
-  const theContext = useContext(GlobalContext);
-  const {
-    contextState: { title, counter },
-  } = theContext;
-  return (
-    <h1>
-      {title} {counter}
-    </h1>
-  );
-};
-
-const P = () => {
-  const theContext = useContext(GlobalContext);
-  const {
-    contextState: { body },
-    setContextState,
-  } = theContext;
-  return <p onClick={() => setContextState((s) => ({ ...s, counter: s.counter + 1 }))}>{body}</p>;
-};
+import React from 'react';
+import { AppContext } from './contexts/AppContext';
+import { Div } from './components/Div/index';
 
 function UseContext() {
-  const [contextState, setContextState] = useState(globalState);
   return (
-    <GlobalContext.Provider value={{ contextState, setContextState }}>
+    <AppContext>
       <Div />
-    </GlobalContext.Provider>
+    </AppContext>
   );
 }
 
