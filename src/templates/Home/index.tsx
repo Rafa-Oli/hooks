746e61459dbab5c5c3c78bdd8b25cb7b7721df98
Rafaela Oliveira <rafaela.albaniza@gmail.com>
2021-10-18 21:29:35 -0300

    featr: create hook custom

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,40 +1,95 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+
+const isObjectEqual = (objA, objB) => {
+  return JSON.stringify(objA) === JSON.stringify(objB);
+};
 
 const useFetch = (url, options) => {
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [shouldLoad, setShouldLoad] = useState(false);
+  const urlRef = useRef(url);
+  const optionsRef = useRef(options);
 
   useEffect(() => {
+    let changed = false;
+    if (!isObjectEqual(url, urlRef.current)) {
+      urlRef.current = url;
+      changed = true;
+    }
+    if (!isObjectEqual(options, optionsRef.current)) {
+      optionsRef.current = options;
+      changed = true;
+    }
+
+    if (changed) {
+      setShouldLoad((s) => !s);
+    }
+  }, [url, options]);
+
+  useEffect(() => {
+    let wait = false;
     setLoading(true);
     const fetchData = async () => {
-      await new Promise((r) => setTimeout(r, 3000));
+      await new Promise((r) => setTimeout(r, 1000));
       try {
-        const response = await fetch(url, options);
+        const response = await fetch(urlRef.current, optionsRef.current);
         const jsonResult = await response.json();
-        setResult(jsonResult);
-        setLoading(false);
+        if (!wait) {
+          setResult(jsonResult);
+          setLoading(false);
+        }
       } catch (e) {
-        setLoading(false);
+        if (!wait) {
+          setLoading(false);
+        }
         throw e;
       }
     };
     fetchData();
-  }, [url]);
+    return () => {
+      wait = true;
+    };
+  }, [shouldLoad]);
 
   return [result, loading];
 };
 
 export const Home = () => {
-  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts', {
+  const [postId, setPostId] = useState('');
+  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts/' + postId, {
     method: 'GET',
     headers: {
       abc: '1',
     },
   });
+  useEffect(() => {
+    console.log('post id', postId);
+  }, [postId]);
+
+  const handleClick = (id) => {
+    setPostId(id);
+  };
 
   if (loading) {
     return <p>Loading ....</p>;
   }
 
-  return <h1>Oi</h1>;
+  if (!loading && result) {
+    return (
+      <div>
+        {result?.length > 0 ? (
+          result.map((p) => (
+            <div key={p.id} onClick={() => handleClick(p.id)}>
+              <p>{p.title}</p>
+            </div>
+          ))
+        ) : (
+          <div onClick={() => handleClick('')}>
+            <p>{result.title}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
 };
